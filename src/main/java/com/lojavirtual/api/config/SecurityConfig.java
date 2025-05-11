1aae2666b7b1dd171b4f59f861a6ae1da7ab4373
devvvvvs <dicasdeacademia@gmail.com>
2025-05-11 19:22:58 -0300

    commit

--- src/main/java/com/lojavirtual/api/config/SecurityConfig.java
+++ src/main/java/com/lojavirtual/api/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.lojavirtual.api.config;
 // SecurityConfig.java
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,6 +11,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
@@ -17,6 +23,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf().disable() // comum em APIs REST
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/produto/**").hasRole("ADMIN") // exige role ADMIN
@@ -26,6 +33,22 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5173",
+                "https://adm-lojavirtual.vercel.app"
+        ));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // aplica globalmente
+        return source;
+    }
+
 
     // Usuário em memória (comum em dev/teste; no mercado isso viria de banco de dados ou LDAP)
     @Bean
