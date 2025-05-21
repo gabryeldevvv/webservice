357cd9e4e65d5e58b3c297b7b4c9369515191d96
devvvvvs <dicasdeacademia@gmail.com>
2025-05-21 11:02:16 -0300

    commit

--- src/main/java/com/lojavirtual/api/config/SecurityConfig.java
+++ src/main/java/com/lojavirtual/api/config/SecurityConfig.java
@@ -23,7 +23,8 @@ public class SecurityConfig {
 
     private static final List<String> ALLOWED_ORIGINS = Arrays.asList(
             "http://localhost:5173",
-            "https://adm-lojavirtual.vercel.app"
+            "https://adm-lojavirtual.vercel.app",
+            "https://ecommerce-5a44.vercel.app"
     );
 
     private static final List<String> ALLOWED_METHODS = Arrays.asList(
